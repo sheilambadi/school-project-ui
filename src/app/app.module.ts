d9c0acb576a10b5d36ef4ca56af69feaa750d390
sheilambadi <mbadiatieno@gmail.com>
2018-09-16 09:47:31 +0300

    Add components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
+import { ExamsComponent } from './exams/exams.component';
+import { ResultsComponent } from './results/results.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    ExamsComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule
