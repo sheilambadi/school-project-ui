857f5f3511641ef4db82f4c7957d8dc2cf50e60f
sheilambadi <mbadiatieno@gmail.com>
2018-09-19 15:19:43 +0300

    Select exam to display results

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -9,12 +9,28 @@ import { Component, OnInit } from '@angular/core';
 export class ResultsComponent implements OnInit {
 
   public results = [];
+  exams;
+  examId;
 
   constructor(private service: SchoolService) {
   }
 
   ngOnInit() {
     this.service.getResultsByExam(1).subscribe(data => {
+      this.results = data;
+      // console.log(data);
+    });
+
+    this.service.getExams().subscribe(data => {
+      this.exams = data;
+      // console.log(data);
+    });
+  }
+
+  onChange(examId) {
+    this.examId = examId;
+
+    this.service.getResultsByExam(examId).subscribe(data => {
       this.results = data;
       // console.log(data);
     });
